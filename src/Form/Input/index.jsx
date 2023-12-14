86e2ff33655e849466048ae7e467177423ddf7df
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2023-12-14 11:42:49 +0000

    Form-Data

--- src/Form/Input/index.jsx
+++ src/Form/Input/index.jsx
@@ -1,32 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useRef } from 'react';
 import styles from './index.module.scss';
-// import { labels } from '../../data';
+import Form from '..';
+
+const FormData = [
+  { name: 'Name', surname: 'surname', age: 'age', button: 'Submit' },
+  { name: 'Name', surname: 'surname', age: 'age', button: 'Submit' },
+  { name: 'Name', surname: 'surname', age: 'age', button: 'Submit' },
+];
 
 const Input = () => {
-  const [userName, setUserName] = useState(() => {
-    const savedItem = localStorage.getItem('userName');
-    const parsedItem = JSON.parse(savedItem);
-    return parsedItem || '';
-  });
-
-  useEffect(() => {
-    localStorage.setItem('Username', JSON.stringify(userName));
-  }, [userName]);
-
+  const data = useRef();
+  const handleClick = (e) => {
+    console.log(data.current.value, 'initial value');
+    localStorage.setItem('inputValue', data.current.value);
+    // e.preventDafult();
+  };
+  console.log(localStorage.getItem('inputValue'), '****');
   return (
-    <div>
-      <form className={styles.forms} onSubmit={(e) => e.preventDefault()}>
-        <span>Name</span>
-        <input
-          type='text'
-          value={userName}
-          placeholder='Name'
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <button type='submit' value='submit'>
-          Submit
-        </button>
-      </form>
+    <div className={styles.form}>
+      {FormData.map((item, index) => (
+        <form key={index}>
+          <span>{item.name}</span>
+          <input type='text' placeholder={item.name} ref={data} />
+          <button type='submit' onClick={handleClick}>
+            {item.button}
+          </button>
+        </form>
+      ))}
     </div>
   );
 };
