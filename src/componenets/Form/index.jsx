0a5e4ce16194dab60cfcfb3029f2ca330b434c5a
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2023-12-20 15:42:31 +0000

    useEffect-storage

--- src/componenets/Form/index.jsx
+++ src/componenets/Form/index.jsx
@@ -1,25 +1,116 @@
 import styles from './index.module.scss';
-import { useState } from 'react';
-import Input from '../Input';
-import Heading from '../Heading';
+import { useState, useEffect } from 'react';
 
-const Form = () => {
-  const [name, setName] = useState('');
-  const [surname, setSurname] = useState('');
-  const [age, setAge] = useState('');
+const Form = ({ toggleGetData }) => {
   return (
     <div className={styles.hero}>
-      <Heading
-        name={name}
-        setName={setName}
-        surname={surname}
-        setSurname={setSurname}
-        age={age}
-        setAge={setAge}
-      />
-      <Input />
+      <Heading />
+      <Input toggleGetData={toggleGetData} />
     </div>
   );
 };
 
+// The top div with the heading and stored data
+const Heading = () => {
+  // const [data, setData] = useState('');
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [age, setAge] = useState('');
+  // const [data, setData] = useState('');
+
+  let userInfo = '';
+  const toggleGetData = () => {
+    userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    setName(userInfo.Name);
+    setSurname(userInfo.Surname);
+    setAge(userInfo.Age);
+  };
+
+  ///  try to use useEffect to show data back
+
+  useEffect(() => {
+    toggleGetData();
+  });
+
+  return (
+    <div className={styles.heading}>
+      <h2>User Info</h2>
+      <div>
+        <div>
+          <div>{name}</div>
+          <div> {surname}</div>
+          <div> {age}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Input div with all the data to submit
+const Input = () => {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [age, setAge] = useState('');
+  // const [data, setData] = useState('');
+
+  ///  Function for submit form and store in local storage
+  const handleSubbmission = () => {
+    localStorage.clear('userInfo');
+    let userData = {
+      Name: name,
+      Surname: surname,
+      Age: age,
+    };
+
+    localStorage.setItem('userInfo', JSON.stringify(userData));
+    alert('Data recorded');
+    window.location.reload();
+  };
+
+  ////////////////////////////
+
+  // Function to get items back and show in divs
+
+  return (
+    <div className={styles.form}>
+      <div>
+        <div>
+          <span>Name </span>
+          <input
+            type='text'
+            // name={name}
+            placeholder='Name'
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <span>Surname </span>
+          <input
+            type='text'
+            // name={surname}
+            placeholder='Surname'
+            onChange={(e) => setSurname(e.target.value)}
+          />
+        </div>
+        <div>
+          <span>Age</span>
+          <input
+            type='text'
+            name={age}
+            placeholder='Age'
+            onChange={(e) => setAge(e.target.value)}
+          />
+          <button
+            type='submit'
+            onClick={() => {
+              handleSubbmission();
+            }}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Form;
--- src/componenets/Input/index.jsx
+++ src/componenets/Input/index.jsx
@@ -1,42 +1,75 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 import PropTypes from 'prop-types';
 
-const Input = (props) => {
+/// Heading <component></component>
+
+///////////////////////////////////
+// const Heading = () => {
+//   const [name, setName] = useState('');
+//   const [surname, setSurname] = useState('');
+//   const [age, setAge] = useState('');
+//   const [data, setData] = useState('');
+//   // const [data, setData] = useState('');
+
+//   let userInfo = '';
+//   const toggleGetData = () => {
+//     setData(true);
+//     userInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+//     setName(userInfo.Name);
+//     setSurname(userInfo.Surname);
+//     setAge(userInfo.Age);
+//   };
+//   return (
+//     <div className={styles.heading}>
+//       <h2>User Info</h2>
+//       <div>
+//         <button style={{ marginRight: '5px' }} onClick={toggleGetData}>
+//           Get Data
+//         </button>
+//         {/* <button onClick={toggleEditData}>Edit Data</button> */}
+//         {data && (
+//           <>
+//             <div to>
+//               <div>{name}</div>
+//               <div> {surname}</div>
+//               <div> {age}</div>
+//             </div>
+//           </>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+const Input = () => {
   const [name, setName] = useState('');
   const [surname, setSurname] = useState('');
   const [age, setAge] = useState('');
-  // const [data, setData] = useState(false);
-  // const [editData, setEditData] = useSta0te(true);
-  // console.log(editData);
+  // const [data, setData] = useState('');
 
   ///  Function for submit form and store in local storage
   const handleSubbmission = () => {
-    // localStorage.clear();
+    localStorage.clear('userInfo');
     let userData = {
       Name: name,
       Surname: surname,
       Age: age,
     };
+
     localStorage.setItem('userInfo', JSON.stringify(userData));
     alert('Data recorded');
     window.location.reload();
   };
 
-  // let userInfo = '';
-  // const toggleGetData = () => {
-  //   setData(true);
-  //   userInfo = JSON.parse(localStorage.getItem('userInfo'));
+  ///////////////////////////
+  const counter = '';
+  useEffect(() => {
+    console.log('Counter value: ', counter);
+  }, [counter]);
+  // Function to get items back and show in divs
 
-  //   setName(userInfo.Name);
-  //   setSurname(userInfo.Surname);
-  //   setAge(userInfo.Age);
-  // };
-
-  // function toggleEditData() {
-  //   setEditData(true);
-  //   console.log(editData);
-  // }
   return (
     <div className={styles.form}>
       <div>
@@ -66,7 +99,13 @@ const Input = (props) => {
             placeholder='Age'
             onChange={(e) => setAge(e.target.value)}
           />
-          <button type='submit' onClick={handleSubbmission}>
+          <button
+            type='submit'
+            onClick={() => {
+              handleSubbmission();
+              // toggleGetData();
+            }}
+          >
             Submit
           </button>
         </div>
@@ -80,3 +119,22 @@ Input.propTypes = {
 };
 
 export default Input;
+
+// let userInfo = '';
+// const toggleGetData = () => {
+//   setData(true);
+//   userInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+//   setName(userInfo.Name);
+//   setSurname(userInfo.Surname);
+//   setAge(userInfo.Age);
+// };
+
+// function toggleEditData() {
+//   setEditData(true);
+//   console.log(editData);
+// }
+
+// const [data, setData] = useState(false);
+// const [editData, setEditData] = useSta0te(true);
+// console.log(editData);
