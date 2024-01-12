77768b9b83fad2e96001927273eb5aa7d385ab0a
Lukasz Nogaj <lukasznogaj@Lukaszs-MacBook-Pro.local>
2024-01-12 19:36:02 +0000

    local-storage-struggle

--- src/componenets/Form/index.jsx
+++ src/componenets/Form/index.jsx
@@ -1,81 +1,90 @@
-import styles from './index.module.scss';
-import { useState, useEffect } from 'react';
-import Heading from '../Heading';
+import styles from "./index.module.scss";
+import { useState, useEffect } from "react";
+import { Cookies } from "react-cookie";
+import Heading from "../Heading";
 
 const Form = ({ toggleGetData }) => {
-  return (
-    <div className={styles.hero}>
-      <Heading toggleGetData={toggleGetData} />
-      <Input />
-    </div>
-  );
+	return (
+		<div className={styles.hero}>
+			<Heading toggleGetData={toggleGetData} />
+			<Input />
+		</div>
+	);
 };
 
 /////////////////////////
 
 // Input div with all the data to submit
 const Input = () => {
-  const [name, setName] = useState('');
-  const [surname, setSurname] = useState('');
-  const [age, setAge] = useState('');
-  // const [data, setData] = useState('');
+	const [name, setName] = useState("");
+	const [surname, setSurname] = useState("");
+	const [age, setAge] = useState("");
+	// const [data, setData] = useState('');
 
-  ///  Function for submit form and store in local storage
-  const handleSubbmission = () => {
-    localStorage.clear();
-    let userData = {
-      Name: name,
-      Surname: surname,
-      Age: age,
-    };
+	///  Function for submit form and store in local storage
+	const handleSubbmission = () => {
+		localStorage.clear();
+		let userData = {
+			Name: name,
+			Surname: surname,
+			Age: age,
+		};
 
-    localStorage.setItem('userInfo', JSON.stringify(userData));
-    alert('Data recorded');
-    window.location.reload();
-  };
+		localStorage.setItem("userInfo", JSON.stringify(userData));
+		alert("Data recorded");
+		window.location.reload();
 
-  ////////////////////////////
+		window.addEventListener(
+			"beforeunload",
+			function (e) {
+				this.localStorage.clear();
+			},
+			false
+		);
+	};
 
+	////////////////////////////
+	// Function to set cookies
 
-  // Function to get items back and show in divs
+	// Function to get items back and show in divs
 
-  return (
-    <div className={styles.form}>
-      <div>
-        <div className={styles.enteredDate}>
-          <span> Name</span>
-          <input
-            type='text'
-            // name={name}
-            placeholder='John'
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className={styles.enteredDate}>
-          <span>Surname </span>
-          <input
-            type='text'
-            // surname={surname}
-            placeholder='Handsome'
-            onChange={(e) => setSurname(e.target.value)}
-          />
-        </div>
-        <div className={styles.enteredDate}>
-          <span>Age</span>
-          <input
-            type='text'
-            // age={age}
-            placeholder='41'
-            onChange={(e) => setAge(e.target.value)}
-          />
-        </div>
-      </div>
-      <div>
-        <button type='submit' disabled={!name} onClick={handleSubbmission}>
-          Submit
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.form}>
+			<div>
+				<div className={styles.enteredDate}>
+					<span> Name</span>
+					<input
+						type="text"
+						// name={name}
+						placeholder="John"
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
+				<div className={styles.enteredDate}>
+					<span>Surname </span>
+					<input
+						type="text"
+						// surname={surname}
+						placeholder="Handsome"
+						onChange={(e) => setSurname(e.target.value)}
+					/>
+				</div>
+				<div className={styles.enteredDate}>
+					<span>Age</span>
+					<input
+						type="text"
+						// age={age}
+						placeholder="41"
+						onChange={(e) => setAge(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div>
+				<button type="submit" disabled={!name} onClick={handleSubbmission}>
+					Submit
+				</button>
+			</div>
+		</div>
+	);
 };
 export default Form;
--- src/componenets/Heading/index.jsx
+++ src/componenets/Heading/index.jsx
@@ -1,48 +1,45 @@
-import { useState,useEffect } from 'react';
-import styles from './index.module.scss';
-
+import { useState, useEffect } from "react";
+import styles from "./index.module.scss";
 
 const Heading = () => {
-  const [name, setName] = useState('');
-  const [surname, setSurname] = useState('');
-  const [age, setAge] = useState('');
+	const [name, setName] = useState("");
+	const [surname, setSurname] = useState("");
+	const [age, setAge] = useState("");
 
-  let userInfo = '';
-  function toggleGetData ()  {
-    userInfo = JSON.parse(localStorage.getItem('userInfo'));
-    setName(userInfo.Name);
-    setSurname(userInfo.Surname);
-    setAge(userInfo.Age);
-  };
+	let userInfo = "";
+	function toggleGetData() {
+		userInfo = JSON.parse(localStorage.getItem("userInfo"));
+		setName(userInfo.Name);
+		setSurname(userInfo.Surname);
+		setAge(userInfo.Age);
+	}
 
-  ///  try to use useEffect to show data back
+	///  try to use useEffect to show data back
 
-  useEffect(() => {
-    toggleGetData();
-  });
+	useEffect(() => {
+		toggleGetData();
+	});
 
-  return (
-    
-    <div className={styles.heading}>
-      <div>
-        <h2 className={styles.head}>User Info</h2>
-      </div>
+	return (
+		<div className={styles.heading}>
+			<div>
+				<h2 className={styles.head}>User Info</h2>
+			</div>
 
-      <div className={styles.data}>
-        <div className={styles.user}>
-          <div> Name:</div>
-          <div> Surname:</div>
-          <div> Age:</div>
-        </div>
-        <div className={styles.userData}>
-          <div>{name}</div>
-          <div>{surname}</div>
-          <div>{age}</div>
-        </div>
-      </div>
-    </div>
-    
-  );
+			<div className={styles.data}>
+				<div className={styles.user}>
+					<div> Name - </div>
+					<div> Surname- </div>
+					<div> Age - </div>
+				</div>
+				<div className={styles.userData}>
+					<div>{name}</div>
+					<div>{surname}</div>
+					<div>{age}</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Heading;
