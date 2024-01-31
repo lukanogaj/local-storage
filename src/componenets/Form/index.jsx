a8858ab32332e12ff2064649c1b634d84999c519
Lukasz Nogaj <lukasznogaj@Lukaszs-MacBook-Pro.local>
2024-01-31 13:53:07 +0000

    url

--- src/componenets/Form/index.jsx
+++ src/componenets/Form/index.jsx
@@ -4,59 +4,11 @@ import { Cookies, useCookies } from "react-cookie";
 // import Heading from "../Heading";
 
 const Form = () => {
-	const [name, setName] = useState("");
-	const [surname, setSurname] = useState("");
-	const [age, setAge] = useState("");
-	const [cookies, setCookie] = useCookies(["user"]);
-
-	//// Use function from sibling component
-	// const handleSubbmission = () => {
-	// 	localStorage.clear();
-	// 	let userData = {
-	// 		Name: name,
-	// 		Surname: surname,
-	// 		Age: age,
-	// 	};
-	// 	console.log(typeof userData);
-	// 	// console.log(JSON.stringify(userData));
-
-	// 	/////////////////////////////
-	// 	// Base 64
-	// 	// Get user data from form
-	// 	const jasonObject = JSON.stringify(userData);
-	// 	console.log(jasonObject);
-	// 	// Encode into base64
-	// 	const base64DataEncode = btoa(jasonObject);
-	// 	// Add it as query paremeter user_data
-	// 	const searchParams = new URLSearchParams();
-	// 	searchParams.append("userData", base64DataEncode);
-	// 	console.log(searchParams);
-	// 	// Decode form 64 read the values from query param
-	// 	const myParam = searchParams.get("userData");
-	// 	console.log(myParam);
-	// 	// Decode from base64
-	// 	const decodeUserData = atob(myParam);
-	// 	console.log(decodeUserData);
-	// 	// Parse the JSON Object
-	// 	const parseUserData = JSON.parse(decodeUserData);
-	// 	console.log(parseUserData);
-	// 	// Loop through object
-
-	// 	// console.log(loopObject());
-
-	// 	////////////////////////////////////
-	// 	const handleCookie = () => {
-	// 		setCookie("userData", JSON.stringify(userData), { path: "/" });
-	// 	};
-	// 	handleCookie();
-	// 	localStorage.setItem("userInfo", JSON.stringify(userData));
-	// 	alert("Data recorded");
-	// 	window.location.reload();
-	// };
+	///
 
 	return (
 		<div className={styles.hero}>
-			<Heading name={name} />
+			<Heading />
 			<Input />
 		</div>
 	);
@@ -111,8 +63,8 @@ const Input = () => {
 	///// States for the cookies and  query parametres
 	const [cookies, setCookie] = useCookies(["user"]);
 
-	///  Function for submit form and store in local storage
-	const handleSubbmission = () => {
+	///  Function for submit form and store in local storage, encode and decode in base 64
+	const handleSubmit = () => {
 		localStorage.clear();
 		let userData = {
 			Name: name,
@@ -187,15 +139,10 @@ const Input = () => {
 				</div>
 			</div>
 			<div>
-				<button type="submit" disabled={!name} onClick={handleSubbmission}>
+				<button type="submit" onClick={handleSubmit}>
 					Submit
 				</button>
 			</div>
-			{/* <div>
-				{parseUserData.map((item) => (
-					<div>Hello</div>
-				))}
-			</div> */}
 		</div>
 	);
 };
